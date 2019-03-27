68a95744ff5cbf2f796903657d99c59d57da65a4
Linktech <popov.n294@gmail.com>
2019-03-27 12:02:10 +0300

    Now it works
    (updated #5)

--- src/com/vk/lincharageniy/WallpaperSetter.java
+++ src/com/vk/lincharageniy/WallpaperSetter.java
@@ -15,7 +15,7 @@ public class WallpaperSetter {
         long SPIF_SENDWININICHANGE = 0x02;
 
         //Here is the problem
-        SPI INSTANCE = (SPI) Native.loadLibrary("user32", SPI.class, new HashMap<Object, Object>() {
+        SPI INSTANCE = (SPI) Native.loadLibrary("user32", SPI.class, new HashMap<String, Object>() {
             {
                 put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
                 put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
@@ -30,8 +30,9 @@ public class WallpaperSetter {
         );
     }
 
-    public static void setWallpaper(String image_url) {
-        String path = image_url;
+    public static void setWallpaper() {
+       String path = "C:\\Users\\Nikitka\\Downloads\\Y8clX863V.jpg";
+
 
         SPI.INSTANCE.SystemParametersInfo(
                 new UINT_PTR(SPI.SPI_SETDESKWALLPAPER),
