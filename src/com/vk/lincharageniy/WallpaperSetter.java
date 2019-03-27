e0d45b3f9cf6bcb4acad5676ab184c4119c98ce7
Linktech <popov.n294@gmail.com>
2019-03-27 11:38:25 +0300

    Added setting a wallpaper class Will be fixed later (updated #5)

--- src/com/vk/lincharageniy/WallpaperSetter.java
+++ src/com/vk/lincharageniy/WallpaperSetter.java
@@ -1,19 +1,43 @@
 package com.vk.lincharageniy;
 
-import com.sun.jna.Library;
 import com.sun.jna.Native;
-import com.sun.jna.win32.W32APIOptions;
+        import com.sun.jna.platform.win32.WinDef.UINT_PTR;
+        import com.sun.jna.win32.*;
+        import java.util.HashMap;
 
 public class WallpaperSetter {
 
-    public static interface User32 extends Library {
-        User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
+    public interface SPI extends StdCallLibrary {
 
-        boolean SystemParametersInfo(int one, int two, String s, int three);
+        //from MSDN article
+        long SPI_SETDESKWALLPAPER = 20;
+        long SPIF_UPDATEINIFILE = 0x01;
+        long SPIF_SENDWININICHANGE = 0x02;
+
+        //Here is the problem
+        SPI INSTANCE = (SPI) Native.loadLibrary("user32", SPI.class, new HashMap<Object, Object>() {
+            {
+                put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
+                put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
+            }
+        });
+
+        boolean SystemParametersInfo(
+                UINT_PTR uiAction,
+                UINT_PTR uiParam,
+                String pvParam,
+                UINT_PTR fWinIni
+        );
     }
 
     public static void setWallpaper(String image_url) {
-        User32.INSTANCE.SystemParametersInfo(0x0014, 0, image_url, 1);
+        String path = image_url;
+
+        SPI.INSTANCE.SystemParametersInfo(
+                new UINT_PTR(SPI.SPI_SETDESKWALLPAPER),
+                new UINT_PTR(0),
+                path,
+                new UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
 
     }
 
